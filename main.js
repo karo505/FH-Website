9acc18e7e6f1b45ec0a6c807913cf4e4660cad9e
Karolina <karolina.adres@op.pl>
2021-04-17 14:23:47 +0200

    add DomContentLoaded

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-var menu = document.querySelector(".menu-mobile");
-var ham = document.querySelector(".ham");
-var xIcon = document.querySelector(".xIcon");
-var menuIcon = document.querySelector(".menuIcon");
+document.addEventListener("DOMContentLoaded", function () {
+	var menu = document.querySelector(".menu-mobile");
+	var ham = document.querySelector(".ham");
+	var xIcon = document.querySelector(".xIcon");
+	var menuIcon = document.querySelector(".menuIcon");
 
-ham.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-	if (menu.classList.contains("showMenu")) {
-		menu.classList.remove("showMenu");
-		xIcon.style.display = "none";
-		menuIcon.style.display = "block";
-	} else {
-		menu.classList.add("showMenu");
-		xIcon.style.display = "block";
-		menuIcon.style.display = "none";
+	function toggleMenu() {
+		if (menu.classList.contains("showMenu")) {
+			menu.classList.remove("showMenu");
+			xIcon.style.display = "none";
+			menuIcon.style.display = "block";
+		} else {
+			menu.classList.add("showMenu");
+			xIcon.style.display = "block";
+			menuIcon.style.display = "none";
+		}
 	}
-}
 
-var menuLinks = document.querySelectorAll(".menuLink");
+	ham.addEventListener("click", toggleMenu);
 
-menuLinks.forEach(function (menuLink) {
-	menuLink.addEventListener("click", toggleMenu);
+	var menuLinks = document.querySelectorAll(".menuLink");
+
+	menuLinks.forEach(function (menuLink) {
+		menuLink.addEventListener("click", toggleMenu);
+	});
 });
